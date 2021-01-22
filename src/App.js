8364869b4a0d7abe24c72b5c4b2f5a5c28a8839e
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2021-01-23 01:03:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
+import CalorieCalc from './CalorieCalc'
+import Home from './Home';
+import HomeHindi from './HomeHindi'
+
+import { Exercise } from './Exercise'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
       <Router>
-
+      <NavigationBar />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+          <Route exact path="/CalorieCalc" component={CalorieCalc} />
+          <Route exact path="/HomeHindi" component={HomeHindi} />
         </Switch>
 
       </Router>
